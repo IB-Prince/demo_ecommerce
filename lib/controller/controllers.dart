0242eff9f2dc2491ccd37b2ee6a5a60e2afd7369
Iqbal Prince <ibprince48@gmail.com>
2023-10-04 07:52:17 +0600

    all at once

---
+++ lib/controller/controllers.dart
@@ -0,0 +1,12 @@
+import 'package:demo_ecommerce/controller/auth_controller.dart';
+import 'package:demo_ecommerce/controller/category_controller.dart';
+import 'package:demo_ecommerce/controller/home_controller.dart';
+import 'package:demo_ecommerce/controller/product_controller.dart';
+
+import 'dashboard_controller.dart';
+
+HomeController homeController = HomeController.instance;
+ProductController productController = ProductController.instance;
+DashboardController dashboardController = DashboardController.instance;
+CategoryController categoryController = CategoryController.instance;
+AuthController authController = AuthController.instance;
